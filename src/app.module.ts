612306586e9162a2b3fc2350bd2828ef16402ecd
sarankumar1998 <saranneumann13@gmail.com>
2025-05-23 18:47:06 +0530

    env setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,38 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AuthModule } from './auth/auth.module';
+import { PostModule } from './postFeed/postFeed.module';
+
 import { User } from './Entities/user.Entity';
 import { PostData } from './Entities/post.Entity';
-import { PostModule } from './postFeed/postFeed.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',      
-      password: 'jarvo',      
-      database: 'demo',
-      entities: [User,PostData],
-      synchronize: true,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (cfg: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: cfg.get('DB_HOST')!,
+          port: parseInt(cfg.get('DB_PORT')!, 10),
+          username: cfg.get('DB_USERNAME')!,
+          password: cfg.get('DB_PASSWORD')!,
+          database: cfg.get('DB_NAME')!,
+          entities: [User, PostData],
+          synchronize: true,
+        };
+      },
+
+      inject: [ConfigService],
     }),
+
     AuthModule,
-    PostModule
+    PostModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,26 @@
+// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../Entities/user.Entity';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]),
-    JwtModule.register({
-      secret: 'secret!NestJSw1th#PGAdmin',
-      signOptions: { expiresIn: '1d' }, 
+  imports: [
+    ConfigModule,
+
+    TypeOrmModule.forFeature([User]),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (cfg: ConfigService) => ({
+        secret: cfg.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: cfg.get<string>('JWT_EXPIRES_IN') },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [AuthService],
